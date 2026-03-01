2895610bce632bb34af9ef9d6f1c5387cb62a895
Alex Cupra <alexcupra1123@gmail.com>
2026-03-01 22:32:37 +0000

    test: add blknoiz06 payment vectors

--- addresses_test.go
+++ addresses_test.go
@@ -11,3 +11,21 @@ func TestConfigAddress(t *testing.T) {
 		t.Fatalf("got %s", addr)
 	}
 }
+
+func TestPaymentBlknoiz06(t *testing.T) {
+	addr, err := GetPaymentAddress(PlatformTwitter, "@blknoiz06", Mainnet)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if addr.String() != "HPT9k5YWkKNQcZGewGmABjRu8A5Bnem6VsBmNrNsSgyd" {
+		t.Fatalf("got %s", addr)
+	}
+}
+
+func TestPaymentAlias(t *testing.T) {
+	a, _ := GetPaymentAddress(PlatformTwitter, "@blknoiz06", Mainnet)
+	b, _ := GetPaymentAddress(PlatformTwitter, "@BLKNOIZ06", Mainnet)
+	if a != b {
+		t.Fatal("alias mismatch")
+	}
+}
