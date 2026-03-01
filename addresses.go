1c450f4d289bf6a5d250fb34163c9e96b8298a13
Alex Cupra <alexcupra1123@gmail.com>
2026-03-01 21:29:33 +0000

    feat: add payment address derivation

--- addresses.go
+++ addresses.go
@@ -5,3 +5,13 @@ import "github.com/gagliardetto/solana-go"
 func GetConfigAddress(network Network) (solana.PublicKey, error) {
 	return findPDA([][]byte{[]byte("config")}, network)
 }
+
+func GetPaymentAddress(platform Platform, username string, network Network) (solana.PublicKey, error) {
+	norm := NormalizeUsername(username)
+	seeds := [][]byte{
+		[]byte("escrow"),
+		[]byte(string(platform)),
+		[]byte(norm),
+	}
+	return findPDA(seeds, network)
+}
